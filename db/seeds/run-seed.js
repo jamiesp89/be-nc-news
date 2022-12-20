8cb7e6ed202c4f7525c5ea4219c11f2394ccc930
Jamie <93950259+jamiesp89@users.noreply.github.com>
2022-12-20 14:52:15 +0000

    add comments to run-seed.js file

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,9 +1,18 @@
-const devData = require('../data/development-data/index.js');
-const seed = require('./seed.js');
-const db = require('../connection.js');
+// import development data
+const devData = require("../data/development-data/index.js");
+// import seed function
+const seed = require("./seed.js");
+// import connection to database
+const db = require("../connection.js");
 
 const runSeed = () => {
-  return seed(devData).then(() => db.end());
+  // run the seed function
+  return (
+    seed(devData)
+      // close the database connection
+      .then(() => db.end())
+  );
 };
 
+// run the seed file
 runSeed();
