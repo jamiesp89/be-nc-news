3aea5a48b7f733059162b54d33ef4a1bff956d7e
Jamie <93950259+jamiesp89@users.noreply.github.com>
2022-12-20 14:48:19 +0000

    add comments to connection.js file

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,20 @@
+// require the pg module
 const { Pool } = require("pg");
+
+// set environment variables
 const ENV = process.env.NODE_ENV || "development";
 
+// require the dotenv module
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+// check that the .env file has the required variables
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
+// set the configuration for the database
 const config =
   ENV === "production"
     ? {
@@ -19,4 +25,5 @@ const config =
       }
     : {};
 
+// export the connection to the database
 module.exports = new Pool(config);
